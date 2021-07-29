c4abf69e91139bf50ea0e254d7fb13d058abf91e
Wolfgang Schiffels <wolfgang.schiffels@baqend.com>
2021-07-29 12:06:59 +0200

    there is no such thing as /var/log/supervisor/

--- docker/server/scripts/wpt_cron_call.sh
+++ docker/server/scripts/wpt_cron_call.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www/webpagetest/cron/
 if [ ! -f "${1}" ]; then
-  echo "Script name '${1}' is not a valid WPT cron script." >> /var/log/supervisor/cron_stdout.log
+  echo "Script name '${1}' is not a valid WPT cron script." >> /var/log/cron_stdout.log
   exit 1
 fi
-echo "Run ${1} at [$(date)]" >> /var/log/supervisor/cron_stdout.log
-/usr/local/bin/php "${1}" >> /var/log/supervisor/cron_stdout.log 2>&1
+echo "Run ${1} at [$(date)]" >> /var/log/cron_stdout.log
+/usr/local/bin/php "${1}" >> /var/log/cron_stdout.log 2>&1
